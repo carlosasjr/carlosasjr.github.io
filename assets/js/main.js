11d4825c0f86d8409b07cf004c21ab251b04df17
Carlos Antônio dos Santos Júnior <43422181+carlosasjr@users.noreply.github.com>
2022-02-21 16:29:50 -0300

    add new pwa project

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,28 @@ $(document).ready(function () {
         ]);
     });	
 
+
+    $('#gallery3').on('click', function (event) {
+        event.preventDefault();
+        Fresco.show([
+            { url: 'assets/img/projects/pwa/screenshot-1.png', caption: 'Login' },
+            { url: 'assets/img/projects/pwa/screenshot-2.png', caption: 'New Company' },
+            { url: 'assets/img/projects/pwa/screenshot-3.png', caption: 'Home' },
+            { url: 'assets/img/projects/pwa/screenshot-4.png', caption: 'Menu' },
+            { url: 'assets/img/projects/pwa/screenshot-5.png', caption: 'Side Menu' },
+            { url: 'assets/img/projects/pwa/screenshot-6.png', caption: 'Clients' },
+            { url: 'assets/img/projects/pwa/screenshot-7.png', caption: 'Start New Sale' },            
+            { url: 'assets/img/projects/pwa/screenshot-9.png', caption: 'Products' },
+            { url: 'assets/img/projects/pwa/screenshot-10.png', caption: 'Add Product' },
+            { url: 'assets/img/projects/pwa/screenshot-11.png', caption: 'Checkout' },
+            { url: 'assets/img/projects/pwa/screenshot-12.png', caption: 'Add  Form Payments' },
+            { url: 'assets/img/projects/pwa/screenshot-13.png', caption: 'Checkout with Forms add' },
+            { url: 'assets/img/projects/pwa/screenshot-14.png', caption: 'List Sales' },
+            
+        ]);
+    });	
+
+
  /*   $('#gallery2').on('click', function (event) {
         event.preventDefault();
         Fresco.show([
